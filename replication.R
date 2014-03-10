91981ad0dd1e6d11cb8457b90be052dc2a0ba0b7
ajutca <ajutca@users.noreply.github.com>
2014-03-10 17:59:09 -0400

    Update replication.R

--- replication.R
+++ replication.R
@@ -21,5 +21,33 @@ colnames(mydata, do.NULL = TRUE) #So we can call columns by name
 ##############################
 ##Summary Statistics##########
 ##############################
+###mean(mydata[mydata$a_age>=16 & mydata$a_age<=22 & mydata$year<1997,c("insured","pubhi","privhi", "pub_ng", "privonly", "group","nongroup")])
 
-mean(mydata[mydata$a_age>=16 & mydata$a_age<=22 & mydata$year<1997,c("insured","pubhi","privhi", "pub_ng", "privonly", "group","nongroup")])
+
+##Full Sample
+mydata.sub<-subset(mydata,mydata$year<1997 & mydata$a_age>=16 & mydata$a_age<=22)
+colMeans(subset(mydata.sub, select = c("insured", "pubhi","privhi","pub_ng", "privonly", "group", "nongroup")), na.rm = TRUE)
+
+##Full sample w/ parents
+mydata.parents<-subset(mydata,mydata$year<1997 & mydata$a_age>=16 & mydata$a_age<=22 & a_parent>0)
+colMeans(subset(mydata.parents, select = c("insured", "pubhi","privhi","pub_ng", "privonly", "group", "nongroup")), na.rm = TRUE)
+
+##Income <= 150% of poverty line
+mydata.pov150<-subset(mydata,mydata$year<1997 & mydata$a_age>=16 & mydata$a_age<=22 & a_parent>0 & pov==1.5)
+colMeans(subset(mydata.pov150, select = c("insured", "pubhi","privhi","pub_ng", "privonly", "group", "nongroup")), na.rm = TRUE)
+
+##Income 150% - 300% of poverty line
+mydata.pov300<-subset(mydata,mydata$year<1997 & mydata$a_age>=16 & mydata$a_age<=22 & a_parent>0 & pov==3)
+colMeans(subset(mydata.pov300, select = c("insured", "pubhi","privhi","pub_ng", "privonly", "group", "nongroup")), na.rm = TRUE)
+
+##Income > 300% of poverty line
+mydata.pov400<-subset(mydata,mydata$year<1997 & mydata$a_age>=16 & mydata$a_age<=22 & a_parent>0 & pov==4)
+colMeans(subset(mydata.pov400, select = c("insured", "pubhi","privhi","pub_ng", "privonly", "group", "nongroup")), na.rm = TRUE)
+
+##Group coverage
+mydata.group<-subset(mydata,mydata$year<1997 & mydata$a_age>=16 & mydata$a_age<=22 & a_parent>0 & grouphi_any==1)
+colMeans(subset(mydata.group, select = c("insured", "pubhi","privhi","pub_ng", "privonly", "group", "nongroup")), na.rm = TRUE)
+
+##Non-group coverage
+mydata.nongroup<-subset(mydata,mydata$year<1997 & mydata$a_age>=16 & mydata$a_age<=22 & a_parent>0 & grouphi_any==0)
+colMeans(subset(mydata.nongroup, select = c("insured", "pubhi","privhi","pub_ng", "privonly", "group", "nongroup")), na.rm = TRUE)
